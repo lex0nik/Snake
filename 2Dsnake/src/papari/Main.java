c6bdf48adac2f4d8b45743d71a3a72227cb8b371
lex0nik <lexsps@hotmail.com>
2019-03-19 20:42:02 +0200

    Snake implementation

---
+++ 2Dsnake/src/papari/Main.java
@@ -0,0 +1,21 @@
+package papari;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame obj = new JFrame();
+		Gameplay gameplay = new Gameplay();
+		obj.setBackground(Color.DARK_GRAY);
+		obj.setBounds(10,10, 905, 700);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.setResizable(false);
+		obj.add(gameplay);
+		
+	}
+
+}
